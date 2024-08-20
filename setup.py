5d7a5cb0ee5d831172efed34bd31cd8e67e97343
xchen <xchen@eitm.org>
2024-08-20 16:09:15 +0000

    setup

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     ],
     package_dir={'proteinmpnn':
                  'proteinmpnn',
+                 'proteinmpnn.helper_scripts': 'proteinmpnn/helper_scripts',
                  },
     include_package_data=True,
     install_requires=requirements,
